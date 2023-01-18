b07376b901e03dde36c11829ebb2fbfa530e2ddf
Amy Frieson <ava447@gmail.com>
2023-01-17 20:22:12 -0500

    Modify existing test to not break

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders the application", () => {
+	render(<App />);
+	const appTitle = screen.getByText(/Screen it/i);
+	expect(appTitle).toBeInTheDocument();
 });
